1a83a1cf9878a4362b1d482faecbba7f4a53ef01
Jay Stakelon <stakelon@gmail.com>
2013-06-29 01:38:10 -0700

    specs for client and search

---
+++ spec/redvine_spec.rb
@@ -0,0 +1,51 @@
+require 'redvine'
+require 'spec_helper'
+
+include Helpers
+
+describe Redvine do
+
+  describe '.connect' do
+
+    it 'should create a new client' do
+      expect(Redvine.new).to respond_to(:connect).with(1).argument
+    end
+
+    it 'should raise an error without a username and password' do
+      client = Redvine.new
+      expect { client.connect() }.to raise_error(ArgumentError)
+    end
+    
+    it 'should connect and return a hash with a :vine_key' do
+      config = get_config()
+      client = Redvine.new
+      client.connect(email: config['email'], password: config['password'])
+      expect(client.vine_key).to be_an_instance_of(String)
+    end
+
+  end
+
+  describe '.search' do
+
+    before(:each) do
+      client = setup_client()
+    end
+
+    it 'should respond to a search method' do
+      expect { client }.to respond_to(:search)
+    end
+
+    it 'should throw an error without a hashtag' do
+      expect { client.search() }.to raise_error(ArgumentError)
+    end
+
+    it 'should return a result when searching for a common keyword' do
+      vines = client.search('cat')
+      expect(vines.count).to be > 1
+      expect(vines.first.has_key?('videoUrl')).to be_true
+      expect(vines.first.has_key?('avatarUrl')).to be_true
+    end
+
+  end
+
+end
---
+++ spec/spec_helper.rb
@@ -0,0 +1,12 @@
+module Helpers
+  def setup_client
+    config = get_config()
+    client = Redvine.new
+    client.connect(email: config['email'], password: config['password'])
+  end
+
+  def get_config
+    raw_config = File.read('config.yml')
+    config = YAML.load(raw_config)
+  end
+end
